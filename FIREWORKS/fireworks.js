533e22e95b2092cf6c6e9b02d5a1b82790922518
Max Fu <animemax.fu@gmail.com>
2016-07-05 00:03:23 +0000

    fireworks

--- FIREWORKS/fireworks.js
+++ FIREWORKS/fireworks.js
@@ -1,27 +1,54 @@
 var rocket;
-var rocket2;
 var rockets = [];
-
+var gravity = 0.4;
+var fires = [];
+var scaleCounter = 0;
 function setup() {
     var canvas = createCanvas( 1000,1000);
     background("#081217");
+    rocketSound = loadSound("rocket.mp3");
 }
 
 function mousePressed() {
-    rocket = createSprite(mouseX, height - 25, 25, 25);
-    rocket2 = createSprite(0, mouseY, 25, 25);
+    rocket = createSprite(mouseX, height - 25, 12.5, 25);
+    rocketImage = loadImage("https://hydra-media.cursecdn.com/minecraft.gamepedia.com/b/b0/Firework_rocket.png")
+    rocket.addImage(rocketImage);
+    rocket.scale = 0.5;
     rocket.targetHeight = mouseY;
-    rocket.velocity.y = (mouseY - height) / 100;
-    rocket2.velocity.x = mouseX / 100;
+    rocket.velocity.y = -10;
     rockets.push(rocket);
 }
-
 function draw() {
+    background("#081217");
     for (var i = 0; i < rockets.length; i++) {
         var currRocket = rockets[i];
-        if (currRocket.targetHeight <= currRocket.position.y) {
-            // currRocket.remove();
+        if (currRocket.targetHeight >= currRocket.position.y) {
+            var position = currRocket.position
+            currRocket.remove();
+            console.log(position.x)
+            explode(position.x, position.y)
+            currRocket.position.y = height;
         }
     }
+    scaleCounter++;
+    if(scaleCounter == 5){
+        scaleCounter = 0;
+    for (var n = 0; n < fires.length; n++) {
+        fires[n].velocity.y += gravity;
+         fires[n].scale *= 0.9;
+         if(fires[n].scale <= 0.1){
+             fires[n].remove();
+         }
+    }
+    }
     drawSprites();
+}
+function explode(x, y){
+    rocketSound.play();
+    for(var a = 0; a < 360; a++){
+        fire = createSprite(x,y,5,5);
+        var r = random(360);
+        fire.setSpeed(random(5,10),r);
+        fires.push(fire)
+    }
 }
--- Platformer/game.js
+++ Platformer/game.js
@@ -5,6 +5,7 @@ var GROUND_SPRITE_WIDTH = 50;
 var GROUND_SPRITE_HEIGHT = 50;
 var numGroundSprites;
 var n = 0;
+var bar = 125;
 function setup(){
     var canvas = createCanvas(1500,900);
     canvas.parent('game');
@@ -19,27 +20,40 @@ function setup(){
     var groundSprite = createSprite(n+ 900, height-250, 500,50);
         groundSprites.add(groundSprite);
     player = createSprite(150, height-75, 25, 50);
-    health = createSprite(player.position.x , player.position.y - 75.5, 125, 15);
+    player2 = createSprite(250, height-75, 25, 50);
+    health = createSprite(player.position.x , player.position.y - 75.5, bar, 15);
+    health2 = createSprite(player2.position.x , player2.position.y - 75.5, bar, 15);
 }
 
 function mousePressed(){
     var direction = calcDirection(mouseX-width/2, height/2-mouseY );
     bullets = createSprite(player.position.x,player.position.y,10,10);
     bullets.setSpeed(10,direction);
+    bullets2 = createSprite(player2.position.x,player2.position.y,10,10);
+    bullets2.setSpeed(10,direction);
 }
 function draw(){
     background(150, 200, 250);
     camera.position.x = player.position.x;
     camera.position.y = player.position.y;
     player.velocity.y = player.velocity.y + gravity;
+    player2.velocity.y = player2.velocity.y + gravity;
     health.position.x = player.position.x;
     health.position.y = player.position.y -50;
+    health2.position.x = player2.position.x;
+    health2.position.y = player2.position.y -50;
     textSize(32);
     text("hai", player.position.x-25,player.position.y-75);
+    text("im not gosu", player2.position.x-25,player2.position.y-75);
         if(groundSprites.overlap(player)){
             jumptimes = 0;
             player.velocity.y = 0;
             player.position.y = (player.position.y+50) - (player.height+0.1);
+        }
+        if(groundSprites.overlap(player2)){
+            jumptimes = 0;
+            player2.velocity.y = 0;
+            player2.position.y = (player2.position.y+50) - (player2.height+0.1);
         }
         if((keyDown(UP_ARROW)) && jumptimes < 4){
             player.velocity.y = JUMP;
@@ -51,8 +65,21 @@ function draw(){
         if(keyDown(LEFT_ARROW)){
             player.position.x = player.position.x -5;
         }
+            if(key === 'w'){
+        player2.velocity.y = JUMP;     
+    }
+    if(key === 'a'){
+        player2.position.x = player2.position.x -5;  
+    }
+    if(key === 'd'){
+        player2.position.x = player2.position.x + 5;
+    }
    drawSprites();
 }
+function keyTyped() {
+    
+}
+
 function calcDirection(difX,difY){
     var a =(difX);
     console.log("deltaX:",a);
