bfc2409b591e5a12f1f9a8e80a18002ade8cc1c1
Max Fu <animemax.fu@gmail.com>
2016-07-09 00:06:13 +0000

    initial

--- Platformer/game.js
+++ Platformer/game.js
@@ -6,93 +6,76 @@ var GROUND_SPRITE_HEIGHT = 50;
 var numGroundSprites;
 var n = 0;
 var bar = 125;
-function setup(){
-    var canvas = createCanvas(1500,900);
+var PlayerObject;
+function setup() {
+    var canvas = createCanvas(1500, 900);
     canvas.parent('game');
     background(150, 200, 250);
     groundSprites = new Group();
-    numGroundSprites = width/GROUND_SPRITE_WIDTH + 1;
-        var groundSprite = createSprite(n, height-25, 25000,50);
-            groundSprites.add(groundSprite);
-            
-    var groundSprite = createSprite(n+ 150, height-250, 500,50);
-        groundSprites.add(groundSprite);
-    var groundSprite = createSprite(n+ 900, height-250, 500,50);
-        groundSprites.add(groundSprite);
-    player = createSprite(150, height-75, 25, 50);
-    player2 = createSprite(250, height-75, 25, 50);
-    health = createSprite(player.position.x , player.position.y - 75.5, bar, 15);
-    health2 = createSprite(player2.position.x , player2.position.y - 75.5, bar, 15);
+    numGroundSprites = width / GROUND_SPRITE_WIDTH + 1;
+    var groundSprite = createSprite(n, height - 25, 25000, 50);
+    groundSprites.add(groundSprite);
+
+    var groundSprite = createSprite(n + 150, height - 250, 500, 50);
+    groundSprites.add(groundSprite);
+    var groundSprite = createSprite(n + 900, height - 250, 500, 50);
+    groundSprites.add(groundSprite);
+    player = createSprite(150, height - 75, 25, 50);
+    health = createSprite(player.position.x, player.position.y - 75.5, bar, 15);
+    PlayerObject = {
+        name: "hai",
+        positionX: player.position.x,
+        positionY: player.position.y,
+    };
+}
+function mousePressed() {
+    var direction = calcDirection(mouseX - width / 2, height / 2 - mouseY);
+    bullets = createSprite(player.position.x, player.position.y, 10, 10);
+    bullets.setSpeed(10, direction);
 }
 
-function mousePressed(){
-    var direction = calcDirection(mouseX-width/2, height/2-mouseY );
-    bullets = createSprite(player.position.x,player.position.y,10,10);
-    bullets.setSpeed(10,direction);
-    bullets2 = createSprite(player2.position.x,player2.position.y,10,10);
-    bullets2.setSpeed(10,direction);
-}
-function draw(){
+function draw() {
     background(150, 200, 250);
     camera.position.x = player.position.x;
     camera.position.y = player.position.y;
     player.velocity.y = player.velocity.y + gravity;
-    player2.velocity.y = player2.velocity.y + gravity;
     health.position.x = player.position.x;
-    health.position.y = player.position.y -50;
-    health2.position.x = player2.position.x;
-    health2.position.y = player2.position.y -50;
+    health.position.y = player.position.y - 50;
     textSize(32);
-    text("hai", player.position.x-25,player.position.y-75);
-    text("im not gosu", player2.position.x-25,player2.position.y-75);
-        if(groundSprites.overlap(player)){
-            jumptimes = 0;
-            player.velocity.y = 0;
-            player.position.y = (player.position.y+50) - (player.height+0.1);
-        }
-        if(groundSprites.overlap(player2)){
-            jumptimes = 0;
-            player2.velocity.y = 0;
-            player2.position.y = (player2.position.y+50) - (player2.height+0.1);
-        }
-        if((keyDown(UP_ARROW)) && jumptimes < 4){
-            player.velocity.y = JUMP;
-            jumptimes++;
-        }
-        if(keyDown(RIGHT_ARROW)){
-            player.position.x = player.position.x + 5;
-        }
-        if(keyDown(LEFT_ARROW)){
-            player.position.x = player.position.x -5;
-        }
-            if(key === 'w'){
-        player2.velocity.y = JUMP;     
+    text("hai", player.position.x - 25, player.position.y - 75);
+    if (groundSprites.overlap(player)) {
+        jumptimes = 0;
+        player.velocity.y = 0;
+        player.position.y = (player.position.y + 50) - (player.height + 0.1);
     }
-    if(key === 'a'){
-        player2.position.x = player2.position.x -5;  
+    if ((keyDown(UP_ARROW)) && jumptimes < 4) {
+        player.velocity.y = JUMP;
+        jumptimes++;
     }
-    if(key === 'd'){
-        player2.position.x = player2.position.x + 5;
+    if (keyDown(RIGHT_ARROW)) {
+        player.position.x = player.position.x + 5;
     }
-   drawSprites();
-}
-function keyTyped() {
-    
+    if (keyDown(LEFT_ARROW)) {
+        player.position.x = player.position.x - 5;
+    }
+    drawSprites();
 }
 
-function calcDirection(difX,difY){
-    var a =(difX);
-    console.log("deltaX:",a);
-    var b =(difY);
-    console.log("deltaY:",b);
-    var c = Math.atan(b/a);
-        if(mouseX < width/2){
-            var c = Math.atan(-b/a) -Math.PI;
+function keyTyped() {
+
+}
+
+function calcDirection(difX, difY) {
+    var a = (difX);
+    var b = (difY);
+    var c = Math.atan(b / a);
+    if (mouseX < width / 2) {
+        var c = Math.atan(-b / a) - Math.PI;
     }
-    else{
-        var c = Math.atan(b/-a);
+    else {
+        var c = Math.atan(b / -a);
     }
-   var d = c*180/ Math.PI;
+    var d = c * 180 / Math.PI;
     console.log(d);
     return d;
 }
--- Platformer/main.js
+++ Platformer/main.js
@@ -1,36 +1,59 @@
-  var config = {
-    apiKey: "AIzaSyCeylwYo1QMoVyITviu-7_SOb8GlyWdfr8",
-    authDomain: "chat-app-ee1ec.firebaseapp.com",
-    databaseURL: "https://chat-app-ee1ec.firebaseio.com",
-    storageBucket: "chat-app-ee1ec.appspot.com",
-  };
-firebase.initializeApp(config);
+ var users;
+ var UId;
+ var playerArray = [];
+ var playerSprites = [];
+ var config = {
+  apiKey: "AIzaSyCeylwYo1QMoVyITviu-7_SOb8GlyWdfr8",
+  authDomain: "chat-app-ee1ec.firebaseapp.com",
+  databaseURL: "https://chat-app-ee1ec.firebaseio.com",
+  storageBucket: "chat-app-ee1ec.appspot.com",
+ };
+ firebase.initializeApp(config);
 
-var chatData = firebase.database().ref();
-function pushMessage(event){
- if(event.keyCode == 13){
-    var name = $('#nameInput').val();
-    var text = $('#messageInput').val();
-    chatData.push({name: name, text: text});
-    $('#messageInput').val(''); 
+ var chatData = firebase.database().ref('chat');
+ var userData = firebase.database().ref('users');
+userData.on("child_added",playersAdded);
+function playersAdded(newPlayer) {
+ playerArray.push(newPlayer.val());
+ console.log(newPlayer.val());
+ playerSprites.push(createSprite(newPlayer.val().positionX,newPlayer.val().positionY,25,50));
+ 
+}
+ function pushMessage(event) {
+  if (event.keyCode == 13) {
+   var name = $('#nameInput').val();
+   var text = $('#messageInput').val();
+   chatData.push({
+    name: name,
+    text: text
+   });
+   $('#messageInput').val('');
+  }
  }
-}
-chatData.on("child_added", showMessage);
+ userData.once('value').then(function(snapshot) {
+  users = snapshot.val();
+  console.log(snapshot);
+  console.log(PlayerObject);
+  console.log(users);
+  PlayerObject.UId = Object.keys(users).length;
+    userData.push(PlayerObject);
+ });
+ chatData.on("child_added", showMessage);
 
-function showMessage(msg){
-    var message = msg.val();
-    var messageSender = message.name;
-    var messageContent = message.text;
-    
-    var messageE1 = $("<div/>").addClass("message");
-    var senderE1 = $("<span/>").text(messageSender + ":")
-    var contentE1 = $("<span/>").text(messageContent);
-    
-    messageE1.append(senderE1);
-    messageE1.append(contentE1);
-    $('#messages').append(messageE1);
-    console.log(messageE1);
-     console.log(senderE1);
-      console.log(contentE1);
-}
-$('#messageInput').keypress(pushMessage);
+ function showMessage(msg) {
+  var message = msg.val();
+  var messageSender = message.name;
+  var messageContent = message.text;
+
+  var messageE1 = $("<div/>").addClass("message");
+  var senderE1 = $("<span/>").text(messageSender + ":")
+  var contentE1 = $("<span/>").text(messageContent);
+
+  messageE1.append(senderE1);
+  messageE1.append(contentE1);
+  $('#messages').append(messageE1);
+ }
+ $('#messageInput').keypress(pushMessage);
+
+ function ButtonPressed() {}
+ $("#button").on("click", ButtonPressed);
